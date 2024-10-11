98a11b381d16c6ba1c1db85ae91e3a0ad7eca921
mohammed <mohammedhussein004@gmail.com>
2024-10-11 19:27:09 +0300

    add file js & Edite files html & css

---
+++ main.js
@@ -0,0 +1,9 @@
+let icon = document.querySelector(".header .container .links .icon");
+let ul = document.querySelector(".header .container .links ul");
+icon.addEventListener("click", function () {
+  if (ul.style.display === "none") {
+    ul.style.display = "block";
+  } else {
+    ul.style.display = "none";
+  }
+});
