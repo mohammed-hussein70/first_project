04b17ecae1200d6b66f24bcc41b588569fcf0bb5
mohammed <mohammedhussein004@gmail.com>
2024-10-11 19:54:43 +0300

    remove change

--- main.js
+++
@@ -1,9 +0,0 @@
-let icon = document.querySelector(".header .container .links .icon");
-let ul = document.querySelector(".header .container .links ul");
-icon.addEventListener("click", function () {
-  if (ul.style.display === "none") {
-    ul.style.display = "block";
-  } else {
-    ul.style.display = "none";
-  }
-});
